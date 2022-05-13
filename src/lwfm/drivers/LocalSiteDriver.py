d2baaf25eb4d7e5f1e020818f200c3e8f030e4b1
Hughes <503075765@ge.com>
2022-05-13 14:12:20 -0400

    LocalSiteDriver first draft

--- src/lwfm/drivers/LocalSiteDriver.py
+++ src/lwfm/drivers/LocalSiteDriver.py
@@ -1,8 +1,17 @@
 
 import logging
 
-from lwfm.base.Site import Site, SiteAuthDriver, SiteRunDriver
-from lwfm.base.JobStatus import JobStatus
+import os
+import shutil
+import subprocess
+
+from datetime import datetime
+from pathlib import Path
+
+from lwfm.base.Site import Site, SiteAuthDriver, SiteRunDriver, SiteRepoDriver
+from lwfm.base.SiteFileRef import SiteFileRef, RemoteFSFileRef
+from lwfm.base.JobDefn import JobDefn
+from lwfm.base.JobStatus import JobStatus, JobStatusValues
 
 
 
@@ -17,6 +26,7 @@ class LocalJobStatus(JobStatus):
 #************************************************************************************************************************************
 
 class LocalSiteAuthDriver(SiteAuthDriver):
+    # Because this is running locally, we don't actually need any authentication
     def login(self, force: bool=False) -> bool:
         return True
 
@@ -29,14 +39,68 @@ class LocalSiteAuthDriver(SiteAuthDriver):
     def readFromStore(self) -> bool:
         return True
 
+#***********************************************************************************************************************************
 
 class LocalSiteRunDriver(SiteRunDriver):
+    def submitJob(self, jdefn: JobDefn=None) -> JobStatus:
+        # In local jobs, we spawn the job in a new child process
+        jstatus = JobStatus()
+        # Construct our status message and bail out
+        jstatus.setNativeStatusStr(JobStatusValues.PENDING.value)
+        jstatus.setEmitTime(datetime.utcnow())
+        process = jdefn.getEntryPointPath().split()
+        #Emit RUNNING status
+        try:
+            subprocess.run(process, check=True) # This is synchronous, so we wait here until the subprocess is over. Check=True raises an exception on non-zero return values
+            #Emit FINISHING status
+            #Emit COMPLETE status
+            jstatus.setNativeStatusStr(JobStatusValues.COMPLETE.value)
+        except Exception as ex:
+            logging.error("ERROR: Job failed %s" % (ex))
+            #Emit FAILED status
+            jstatus.setNativeStatusStr(JobStatusValues.FAILED.value)
+        return jstatus
+
     def getJobStatus(self, nativeJobId: str) -> JobStatus:
         return JobStatus()
 
     def cancelJob(self, nativeJobId: str) -> bool:
+        try:
+            os.kill()
+        except Exception as ex:
+            logging.error("ERROR: Could not cancel job %d: %s" % (nativeJobId, ex))
         return True
 
+#***********************************************************************************************************************************
+
+class LocalSiteRepoDriver(SiteRepoDriver):
+    
+    def _copyFile(self, fromPath, toPath):
+        try:
+            shutil.copy(fromPath, toPath)
+        except Exception as ex:
+            logging.error("Error copying file: " + str(ex))
+            return False
+        return True
+    
+    def put(self, localRef: Path, siteRef: SiteFileRef) -> SiteFileRef:
+        fromPath = localRef
+        toPath = siteRef.getPath()
+        if not self._copyFile(fromPath, toPath):
+            return False
+        return siteRef        
+
+    def get(self, siteRef: SiteFileRef, localRef: Path) -> Path:
+        fromPath = siteRef.getPath()
+        toPath = localRef
+        if not self._copyFile(fromPath, toPath):
+            return False
+        return localRef
+        
+
+    def ls(self, siteRef: SiteFileRef) -> SiteFileRef:
+        path = siteRef.getPath()
+        return os.listdir(path)
 
 #************************************************************************************************************************************
 
@@ -44,7 +108,21 @@ class LocalSiteRunDriver(SiteRunDriver):
 if __name__ == '__main__':
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
-    site = Site("local", LocalSiteAuthDriver(), None)
+    site = Site("local", LocalSiteAuthDriver(), LocalSiteRunDriver(), LocalSiteRepoDriver())
     logging.info(site.getName())
     site.getAuthDriver().login()
     logging.info(site.getAuthDriver().isAuthCurrent())
+
+    jdefn = JobDefn()
+    jdefn.setEntryPointPath("python C:/lwfm/foo.py")
+    runDriver = LocalSiteRunDriver()
+    job = runDriver.submitJob(jdefn)
+
+    #siteRef = RemoteFSFileRef()
+    #siteRef._setPath('C:/lwfm')
+    #print(LocalSiteRepoDriver().ls(siteRef))
+    #path = Path('C:/lwfm/foo2.py')
+    #siteRef._setPath('C:/lwfm/foo3.py')
+    #LocalSiteRepoDriver().put(path, siteRef) # Copy foo2 to foo3
+    #path = Path('C:/lwfm/foo4.py')
+    #LocalSiteRepoDriver().get(siteRef, path) # Copy foo3 to foo4   
