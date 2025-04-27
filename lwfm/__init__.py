a7fbd9d6312a5d44d634ad104c17fb4f42f03e51
Andy Gallo <agallojr@gmail.com>
2025-04-26 22:54:23 -0400

    cleanup

--- lwfm/__init__.py
+++ lwfm/__init__.py
@@ -1,3 +1,40 @@
 """
-lwfm
+lwfm public methods 
 """
+
+# base classes
+from lwfm.base.JobContext import JobContext
+from lwfm.base.JobDefn import JobDefn
+from lwfm.base.JobStatus import JobStatus
+from lwfm.base.Metasheet import Metasheet
+from lwfm.base.Site import Site
+from lwfm.base.WorkflowEvent import WorkflowEvent
+from lwfm.base.Workflow import Workflow
+
+# local Site implementation
+from lwfm.sites.LocalSite import LocalSite
+
+# middleware
+from lwfm.midware.LwfManager import lwfManager
+from lwfm.midware.Logger import logger
+
+# utility
+from lwfm.util.IdGenerator import IdGenerator
+
+
+__all__ = [
+    "JobContext",
+    "JobDefn",
+    "JobStatus",
+    "Metasheet",
+    "Site",
+    "WorkflowEvent",
+    "Workflow",
+
+    "LocalSite",
+
+    "lwfManager",
+    "logger",
+
+    "IdGenerator"
+]
--- run-lwfm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xterm -e "./lwfm.sh" & 
-
-
